bc30fa9c923aed6074a0195a9e598c82c1f224d9
seop <useop0821@gmail.com>
2025-01-05 06:29:42 +0900

    refactor: setter에서 dto 빌더 -> 서비스로 변경

--- src/main/java/hello/hello/yju/service/ChatService.java
+++ src/main/java/hello/hello/yju/service/ChatService.java
@@ -1,7 +1,7 @@
 package hello.hello.yju.service;
 
-import hello.hello.yju.dto.ChatRoomDto;
-import hello.hello.yju.entity.ChatMessage;
+import hello.hello.yju.dto.chat.ChatRoomDto;
+import hello.hello.yju.dto.chat.ChatRoomRequest;
 import hello.hello.yju.entity.ChatRoom;
 import hello.hello.yju.entity.ItemEntity;
 import hello.hello.yju.entity.UserEntity;
@@ -9,15 +9,15 @@ import hello.hello.yju.repository.ChatMessageRepository;
 import hello.hello.yju.repository.ChatRoomRepository;
 import hello.hello.yju.repository.ItemRepository;
 import hello.hello.yju.repository.UserRepository;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ChatService {
 
     private final ChatRoomRepository chatRoomRepository;
@@ -26,63 +26,58 @@ public class ChatService {
     private final ItemRepository itemRepository;
 
     @Transactional
-    public ChatRoomDto createChatRoom(String sellerId, String buyerId, Long itemId) {
-        UserEntity seller = userRepository.findByGoogleId(sellerId);
-        UserEntity buyer = userRepository.findByGoogleId(buyerId);
-        ItemEntity item = itemRepository.findById(itemId)
-                .orElseThrow(() -> new IllegalArgumentException("아이템을 찾지 못했습니다."));
+    public ChatRoomDto createChatRoom(ChatRoomRequest request) {
+        ItemEntity item = itemRepository.findById(request.getItemId())
+                .orElseThrow(() -> new IllegalArgumentException("상품을 찾지 못했습니다."));
+        UserEntity seller = item.getUser();
+        UserEntity buyer = findBuyer(request);
 
         // 동일한 채팅방이 이미 존재하는지 확인
-        Optional<ChatRoom> existingChatRoom = chatRoomRepository.findBySellerAndBuyerAndItem(seller, buyer, item);
-        if (existingChatRoom.isPresent()) {
-            ChatRoom chatRoom = existingChatRoom.get();
-            return convertToDto(chatRoom);
-        }
-
-        // 채팅방이 존재하지 않는 경우 새로운 채팅방 생성
-        ChatRoom newChatRoom = new ChatRoom();
-        newChatRoom.setSeller(seller);
-        newChatRoom.setBuyer(buyer);
-        newChatRoom.setItem(item);
-        ChatRoom savedChatRoom = chatRoomRepository.save(newChatRoom);
-
-        return convertToDto(savedChatRoom);
+        return existingChatRoom(request, seller, buyer, item);
     }
 
-    private ChatRoomDto convertToDto(ChatRoom chatRoom) {
-        ChatRoomDto chatRoomDto = new ChatRoomDto();
-        chatRoomDto.setId(chatRoom.getId());
-        chatRoomDto.setSellerId(chatRoom.getSeller().getGoogleId());
-        chatRoomDto.setBuyerId(chatRoom.getBuyer().getGoogleId());
-        chatRoomDto.setItemId(chatRoom.getItem().getId());
-        return chatRoomDto;
-    }
 
-    public List<ChatMessage> getChatMessages(Long chatRoomId) {
-        ChatRoom chatRoom = chatRoomRepository.findById(chatRoomId)
-                .orElseThrow(() -> new IllegalArgumentException("채팅방을 찾지 못했습니다."));
-        return chatMessageRepository.findByChatRoom(chatRoom);
-    }
-
-    public List<ChatRoom> getUserChatRooms(String userId) {
+    public List<ChatRoomDto> getUserChatRooms(String userId) {
         UserEntity user = userRepository.findByGoogleId(userId);
-        return chatRoomRepository.findBySellerOrBuyer(user);
+        return chatRoomRepository.findBySellerOrBuyer(user).stream()
+                .map(ChatRoomDto::of)
+                .toList();
     }
 
-    public ChatRoom getChatRoom(Long chatRoomId) {
+    public ChatRoomDto getChatRoom(Long chatRoomId) {
         return chatRoomRepository.findById(chatRoomId)
+                .map(ChatRoomDto::of)
                 .orElseThrow(() -> new IllegalArgumentException("채팅방을 찾지 못했습니다."));
     }
 
-    public UserEntity getUser(String googleId) {
-        return userRepository.findByGoogleId(googleId);
+    @Transactional
+    public void deleteChatRoom(Long itemId) {
+        List<ChatRoom> chatRooms = chatRoomRepository.findByItem_Id(itemId);
+        deleteChatRooms(chatRooms);
     }
 
-    public void deleteChatRoomsByItemId(Long itemId) {
-        List<ChatRoom> chatRooms = chatRoomRepository.findByItem_Id(itemId);
+    private UserEntity findBuyer(ChatRoomRequest request) {
+        UserEntity buyer = userRepository.findByGoogleId(request.getBuyerId());
+        if (buyer == null) {
+            throw new IllegalArgumentException("구매자를 찾지 못했습니다.");
+        }
+        return buyer;
+    }
+
+    private void deleteChatRooms(List<ChatRoom> chatRooms) {
         for (ChatRoom chatRoom : chatRooms) {
             chatMessageRepository.deleteByChatRoom(chatRoom);
             chatRoomRepository.delete(chatRoom);
         }
+    }
+
+    private ChatRoomDto existingChatRoom(ChatRoomRequest request, UserEntity seller, UserEntity buyer, ItemEntity item) {
+        return chatRoomRepository.findBySellerAndBuyerAndItem(seller, buyer, item)
+                .map(ChatRoomDto::of)
+                .orElseGet(() -> {
+                    ChatRoom newChatRoom = request.toEntity(seller, buyer, item);
+                    ChatRoom savedChatRoom = chatRoomRepository.save(newChatRoom);
+                    return ChatRoomDto.of(savedChatRoom);
+                });
     }
 }
