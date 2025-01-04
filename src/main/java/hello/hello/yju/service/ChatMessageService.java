1860f74b1384a398488545c5b2323c89ea0dae13
seop <useop0821@gmail.com>
2025-01-05 06:29:26 +0900

    refactor: setter에서 dto 빌더 -> 서비스로 변경

--- src/main/java/hello/hello/yju/service/ChatMessageService.java
+++ src/main/java/hello/hello/yju/service/ChatMessageService.java
@@ -1,20 +1,23 @@
 package hello.hello.yju.service;
 
-import hello.hello.yju.dto.ChatMessageDto;
+import hello.hello.yju.dto.chat.ChatMessageDto;
+import hello.hello.yju.dto.chat.ChatMessageRequest;
 import hello.hello.yju.entity.ChatMessage;
 import hello.hello.yju.entity.ChatRoom;
 import hello.hello.yju.entity.UserEntity;
 import hello.hello.yju.repository.ChatMessageRepository;
 import hello.hello.yju.repository.ChatRoomRepository;
 import hello.hello.yju.repository.UserRepository;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class ChatMessageService {
 
     private final ChatMessageRepository chatMessageRepository;
@@ -22,33 +25,42 @@ public class ChatMessageService {
     private final UserRepository userRepository;
 
     @Transactional
-    public ChatMessageDto saveMessage(Long chatRoomId, String message, String senderId, String senderName) {
-        ChatRoom chatRoom = chatRoomRepository.findById(chatRoomId)
+    public ChatMessageDto saveMessage(ChatMessageRequest request) {
+        // 채팅방 검증 및 조회
+        ChatRoom chatRoom = chatRoomRepository.findById(request.getChatRoomId())
                 .orElseThrow(() -> new IllegalArgumentException("채팅방을 찾지 못했습니다."));
 
-        UserEntity sender = userRepository.findByGoogleId(senderId);
+        // 발신자 검증 및 조회
+        UserEntity sender = findSender(request);
+
+        setTime(request);
+
+        ChatMessage chatMessage = request.toEntity(chatRoom, sender);
+        chatMessage = chatMessageRepository.save(chatMessage);
+
+        return ChatMessageDto.of(chatMessage);
+    }
+
+    public List<ChatMessageDto> getChatMessages(Long chatRoomId) {
+        ChatRoom chatRoom = chatRoomRepository.findById(chatRoomId)
+                .orElseThrow(() -> new IllegalArgumentException("채팅방을 찾지 못했습니다."));
+        return chatMessageRepository.findByChatRoom(chatRoom).stream()
+                .map(ChatMessageDto::of)
+                .toList();
+    }
+
+    private static void setTime(ChatMessageRequest request) {
+        if (request.getTimestamp() == null) {
+            request.setTimestamp(LocalDateTime.now());
+        }
+    }
+
+    private UserEntity findSender(ChatMessageRequest request) {
+        UserEntity sender = userRepository.findByGoogleId(request.getSenderId());
         if (sender == null) {
             throw new IllegalArgumentException("발신자를 찾지 못했습니다.");
         }
-
-        ChatMessage chatMessage = new ChatMessage();
-        chatMessage.setChatRoom(chatRoom);
-        chatMessage.setSender(sender);
-        chatMessage.setSenderName(senderName);
-        chatMessage.setMessage(message);
-        chatMessage.setTimestamp(LocalDateTime.now());
-
-        chatMessage = chatMessageRepository.save(chatMessage);
-
-        ChatMessageDto chatMessageDto = new ChatMessageDto();
-        chatMessageDto.setId(chatMessage.getId());
-        chatMessageDto.setChatRoomId(chatMessage.getChatRoom().getId());
-        chatMessageDto.setSenderId(sender.getGoogleId());
-        chatMessageDto.setSenderName(chatMessage.getSenderName());
-        chatMessageDto.setMessage(chatMessage.getMessage());
-        chatMessageDto.setTimestamp(chatMessage.getTimestamp());
-
-
-        return chatMessageDto;
+        return sender;
     }
+
 }
