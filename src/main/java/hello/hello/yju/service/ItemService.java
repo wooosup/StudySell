f12979b54f6410e5c2600df69c536a71e32ab00e
seop <useop0821@gmail.com>
2025-01-05 06:22:26 +0900

    refactor: 메서드 추출, set에서 dto 사용으로 변경

--- src/main/java/hello/hello/yju/service/ItemService.java
+++ src/main/java/hello/hello/yju/service/ItemService.java
@@ -1,28 +1,26 @@
 package hello.hello.yju.service;
 
-import hello.hello.yju.dto.*;
-import hello.hello.yju.entity.ChatRoom;
+import hello.hello.yju.dto.item.ItemFormDto;
+import hello.hello.yju.dto.item.ItemImgDto;
+import hello.hello.yju.dto.item.ItemSearchDto;
+import hello.hello.yju.dto.item.MainItemDto;
 import hello.hello.yju.entity.ItemEntity;
 import hello.hello.yju.entity.ItemImg;
 import hello.hello.yju.entity.UserEntity;
-import hello.hello.yju.repository.ChatRoomRepository;
 import hello.hello.yju.repository.ItemImgRepository;
 import hello.hello.yju.repository.ItemRepository;
 import hello.hello.yju.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
-
 import org.springframework.transaction.annotation.Transactional;
-import jakarta.persistence.EntityNotFoundException;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 
 @Service
@@ -36,30 +34,17 @@ public class ItemService {
     private final UserRepository userRepository;
     private final ChatService chatService;
 
-    public UserEntity getCurrentUserEntityByGoogleId(String googleId) {
-        return userRepository.findByGoogleId(googleId);
-    }
-
     @Transactional
     public Long saveItem(ItemFormDto itemFormDto, List<MultipartFile> itemImgFileList, String googleId) throws Exception {
         UserEntity userEntity = getCurrentUserEntityByGoogleId(googleId);
 
-        ItemEntity itemEntity = itemFormDto.createItem();
+        ItemEntity itemEntity = itemFormDto.toEntity();
         itemEntity.setRegTime(LocalDateTime.now());
-        itemEntity.setUser(userEntity);
+        itemEntity.assignUser(userEntity);
+
         itemRepository.save(itemEntity);
 
-        for (int i = 0; i < itemImgFileList.size(); i++) {
-            ItemImg itemImg = new ItemImg();
-            itemImg.setItem(itemEntity);
-
-            if (i == 0)
-                itemImg.setRepimgYn("Y");
-            else
-                itemImg.setRepimgYn("N");
-
-            itemImgService.saveItemImg(itemImg, itemImgFileList.get(i));
-        }
+        saveItemImg(itemImgFileList, itemEntity);
 
         return itemEntity.getId();
     }
@@ -67,14 +52,14 @@ public class ItemService {
     public ItemFormDto getItemDtl(Long itemId) {
         List<ItemImg> itemImgList = itemImgRepository.findByItemIdOrderByIdAsc(itemId);
 
-        List<ItemImgDto> itemImgDtoList = itemImgList.stream()
-                .map(ItemImgDto::of)
-                .collect(Collectors.toList());
+        List<ItemImgDto> itemImgDtoList = getImgDtos(itemImgList);
 
         ItemEntity itemEntity = itemRepository.findById(itemId)
                 .orElseThrow(EntityNotFoundException::new);
+
         ItemFormDto itemFormDto = ItemFormDto.of(itemEntity);
         itemFormDto.setItemImgDtoList(itemImgDtoList);
+
         return itemFormDto;
     }
 
@@ -82,12 +67,10 @@ public class ItemService {
     public Long updateItem(ItemFormDto itemFormDto, List<MultipartFile> itemImgFileList) throws Exception {
         ItemEntity itemEntity = itemRepository.findById(itemFormDto.getId())
                 .orElseThrow(EntityNotFoundException::new);
+
         itemEntity.updateItem(itemFormDto);
         List<Long> itemImgIds = itemFormDto.getItemImgIds();
-
-        for (int i = 0; i < itemImgFileList.size(); i++) {
-            itemImgService.updateItemImg(itemImgIds.get(i), itemImgFileList.get(i));
-        }
+        updateImg(itemImgFileList, itemImgIds);
 
         return itemEntity.getId();
     }
@@ -96,16 +79,48 @@ public class ItemService {
     public void deleteItem(Long itemId) throws IOException {
         List<ItemImg> itemImgList = itemImgRepository.findByItemIdOrderByIdAsc(itemId);
 
+        findImgList(itemImgList);
+
+        chatService.deleteChatRoom(itemId);
+        itemRepository.deleteById(itemId);
+    }
+
+    public Page<MainItemDto> getMainItemPage(ItemSearchDto itemSearchDto, Pageable pageable){
+        return itemRepository.getMainItemPage(itemSearchDto, pageable);
+    }
+
+    private void saveItemImg(List<MultipartFile> itemImgFileList, ItemEntity itemEntity) throws Exception {
+        for (int i = 0; i < itemImgFileList.size(); i++) {
+            ItemImg itemImg = ItemImg.builder()
+                    .item(itemEntity)
+                    .repimgYn(i == 0 ? "Y" : "N")
+                    .build();
+
+            itemImgService.saveItemImg(itemImg, itemImgFileList.get(i));
+        }
+    }
+
+    private void updateImg(List<MultipartFile> itemImgFileList, List<Long> itemImgIds) throws Exception {
+        for (int i = 0; i < itemImgFileList.size(); i++) {
+            itemImgService.updateItemImg(itemImgIds.get(i), itemImgFileList.get(i));
+        }
+    }
+
+    private void findImgList(List<ItemImg> itemImgList) throws IOException {
         if (!itemImgList.isEmpty()) {
             for (ItemImg itemImg : itemImgList) {
                 itemImgService.deleteItemImg(itemImg);
             }
         }
-
-        chatService.deleteChatRoomsByItemId(itemId);
-        itemRepository.deleteById(itemId);
     }
-    public Page<MainItemDto> getMainItemPage(ItemSearchDto itemSearchDto, Pageable pageable){
-        return itemRepository.getMainItemPage(itemSearchDto, pageable);
+
+    private static List<ItemImgDto> getImgDtos(List<ItemImg> itemImgList) {
+        return itemImgList.stream()
+                .map(ItemImgDto::of)
+                .toList();
+    }
+
+    private UserEntity getCurrentUserEntityByGoogleId(String googleId) {
+        return userRepository.findByGoogleId(googleId);
     }
 }
