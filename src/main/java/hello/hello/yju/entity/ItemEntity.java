4b13faf2547789bea8944b580a5bcb5b1699870b
seop <useop0821@gmail.com>
2025-01-05 06:27:03 +0900

    refactor: set삭제, 액세스 레벨 프로텍터, 빌더 사용

--- src/main/java/hello/hello/yju/entity/ItemEntity.java
+++ src/main/java/hello/hello/yju/entity/ItemEntity.java
@@ -1,13 +1,17 @@
 package hello.hello.yju.entity;
 
-import hello.hello.yju.dto.ItemFormDto;
+import hello.hello.yju.dto.item.ItemFormDto;
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.FetchType.LAZY;
 
 @Entity
 @Getter
-@Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "item")
 public class ItemEntity extends BaseTimeEntity {
 
@@ -24,14 +28,29 @@ public class ItemEntity extends BaseTimeEntity {
 
     private int price;
 
-    private ItemSellStatus itemSellStatus;
+    @Enumerated(EnumType.STRING)
+    private ItemSellStatus itemSellStatus = ItemSellStatus.SELL;
 
     @Transient
     private String relativeTime; // 상대 시간 필드
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "user_id")
     private UserEntity user;
+
+    @Builder
+    private ItemEntity(String department, String itemName, String description, int price, ItemSellStatus itemSellStatus, String relativeTime) {
+        this.department = department;
+        this.itemName = itemName;
+        this.description = description;
+        this.price = price;
+        this.itemSellStatus = itemSellStatus;
+        this.relativeTime = relativeTime;
+    }
+
+    public void assignUser(UserEntity user) {
+        this.user = user;
+    }
 
     public void updateItem(ItemFormDto itemFormDto){
         this.department = itemFormDto.getDepartment();
