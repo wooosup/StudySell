d50409c3e6745c2a56f3ff8900502a75599c8c4e
seop <useop0821@gmail.com>
2025-01-05 06:30:48 +0900

    feat: JpaAuditing 어노테이션 추가

--- src/main/java/hello/hello/yju/YjuApplication.java
+++ src/main/java/hello/hello/yju/YjuApplication.java
@@ -8,6 +8,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @SpringBootApplication
 public class YjuApplication {
 
+
      public static void main(String[] args) {
         SpringApplication.run(YjuApplication.class, args);
     }
