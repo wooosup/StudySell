0df4c6925da2d65bac0fdb7201b9b93fe4f62856
seop <useop0821@gmail.com>
2025-01-05 06:27:45 +0900

    chore: 디렉터리 이동

--- src/main/java/hello/hello/yju/dto/user/CustomOAuth2User.java
+++ src/main/java/hello/hello/yju/dto/user/CustomOAuth2User.java
@@ -1,4 +1,4 @@
-package hello.hello.yju.dto;
+package hello.hello.yju.dto.user;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.oauth2.core.user.OAuth2User;
