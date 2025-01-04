afe8c7177516269a8b9cb0e0565fb1ffb1384347
seop <useop0821@gmail.com>
2025-01-05 06:26:06 +0900

    feat: text와 판매완료 추가

--- src/main/java/hello/hello/yju/entity/ItemSellStatus.java
+++ src/main/java/hello/hello/yju/entity/ItemSellStatus.java
@@ -1,5 +1,14 @@
 package hello.hello.yju.entity;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum ItemSellStatus {
-    SELL , Reservation
+    SELL("판매 중"),
+    Reservation("예약 중"),
+    SOLD_OUT("판매 완료");
+
+    private final String text;
 }
