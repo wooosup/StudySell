f36ed34ae0ef9f25a5cf6c646bdaf6c3e1b51111
seop <useop0821@gmail.com>
2025-01-10 04:48:58 +0900

    feat: 공통 유틸 클래스 생성

---
+++ src/main/java/hello/hello/yju/service/image/FileUtils.java
@@ -0,0 +1,18 @@
+package hello.hello.yju.service.image;
+
+import java.util.UUID;
+
+public class FileUtils {
+
+    public static String createFileName(String originalFileName) {
+        return UUID.randomUUID().toString().concat(getFileExtension(originalFileName));
+    }
+
+    private static String getFileExtension(String fileName) {
+        try {
+            return fileName.substring(fileName.lastIndexOf("."));
+        } catch (StringIndexOutOfBoundsException e) {
+            throw new IllegalArgumentException("잘못된 형식의 파일(" + fileName + ") 입니다.");
+        }
+    }
+}
